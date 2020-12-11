2047917cb3c112ab1186542b75d1708c807e974c
Vaishnavi Gupta <55629345+guptavaishnavi@users.noreply.github.com>
2020-12-11 23:28:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -25,16 +25,9 @@ tl.to('.text2', {y:'10%', duration:2})
 tl.to('.text2', {scale: '1.5',duration:2})
 tl.to('.intro2',{ y:'-100%', duration:2,delay:2})
 tl.to('.intro3',{position:'fixed',duration:0})
-tl.to('.intro3', {x:'100%', duration:2,delay:2})
+tl.to('.intro3', {x:'100%', duration:2,delay:1})
 tl.to('.navbar', {opacity:1, duration:1})
 tl.to('.hero', {opacity:1,duration:1})
+tl.to('.fab', {opacity:1,duration:2})
 
 tl.to('.arrow', { opacity: '100%', y:5, repeat:-1, yoyo:true, duration:1})
-
-
-
-//const tl = gsap.timeline({ default: {ease: 'power4'}})
-
-//
-//tl.to('text', { y:'-10%', duration:2})
-//tl.to('.welcome', {y:'-100%', duration:2,delay:2})
